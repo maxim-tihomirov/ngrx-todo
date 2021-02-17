4884faa977750ca019275ef52bbd5a88a190dc6e
WIN-894K2QHFTQ8\Maks <maxbeere@yandex.ru>
2021-02-17 17:48:23 +0300

    feat: add tests

--- src/app/modules/todo/ui/todo-create-form-ui/todo-create-form-ui.component.spec.ts
+++ src/app/modules/todo/ui/todo-create-form-ui/todo-create-form-ui.component.spec.ts
@@ -1,12 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { TodoCreateFormUiComponent } from './todo-create-form-ui.component';
 
 fdescribe('TodoCreateFormUiComponent', () => {
+  let fixture: ComponentFixture<TodoCreateFormUiComponent>;
   let component: TodoCreateFormUiComponent;
 
   beforeEach(() => {
-    component = new TodoCreateFormUiComponent();
+    TestBed.configureTestingModule({
+      declarations: [TodoCreateFormUiComponent],
+      imports: [FormsModule],
+    });
+    fixture = TestBed.createComponent(TodoCreateFormUiComponent);
+    component = fixture.componentInstance;
+    component.create.emit = jasmine.createSpy();
   });
 
   it('should create', () => {
@@ -18,23 +26,35 @@ fdescribe('TodoCreateFormUiComponent', () => {
   });
 
   it('should not be emit', () => {
-    component.create.subscribe((res: string) => {
-      expect(res).toBe('');
-    });
     component.onCreate();
+    expect(component.create.emit).not.toHaveBeenCalled();
   });
 
   it('should do emit', () => {
     component.name = 'name';
-    component.create.subscribe((res: string) => {
-      expect(res).toBe(component.name);
-    });
     component.onCreate();
+    expect(component.create.emit).toHaveBeenCalled();
   });
 
   it('name should be empty after click', () => {
     component.name = 'name';
     component.onCreate();
     expect(component.name).toBe('');
+  });
+
+  it('should change name in class from DOM', () => {
+    const formElement: HTMLElement = fixture.debugElement.nativeElement;
+    const input = formElement.querySelector('input');
+    input.value = 'name';
+    input.dispatchEvent(new Event('input'));
+    fixture.detectChanges();
+    expect(input.value).toBe(component.name);
+  });
+
+  it('button should be disabled', () => {
+    const formElement: HTMLElement = fixture.debugElement.nativeElement;
+    const button = formElement.querySelector('button');
+    fixture.detectChanges();
+    expect(button.disabled).toBeTruthy();
   });
 });
